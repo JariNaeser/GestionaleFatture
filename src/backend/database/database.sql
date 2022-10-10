efdfb935e8d4a6b0006529fadc87417d693bdd38
Jari Näser <jari.naeser@bluewin.ch>
2022-10-10 20:34:19 +0200

    Create database.sql

--- src/backend/database/database.sql
+++ src/backend/database/database.sql
@@ -1,6 +1,108 @@
-CREATE DATABASE TrackMyFlights;
-USE TrackMyFlights;
+CREATE DATABASE GestionaleFatture;
+USE GestionaleFatture;
 
-CREATE USER 'TMF_Admin'@'localhost' IDENTIFIED BY 'cd0dc2d19261fa53a9c75d6daa68bb13d087474bbd9bab55ea9e1072ef9c53f7'; # plain: admin-salt
-GRANT SELECT, INSERT, UPDATE, DELETE ON TrackMyFlights.* TO 'TMF_Admin'@'localhost';
+CREATE USER 'GF_Admin'@'localhost' IDENTIFIED BY 'cd0dc2d19261fa53a9c75d6daa68bb13d087474bbd9bab55ea9e1072ef9c53f7'; # plain: admin-salt
+GRANT SELECT, INSERT, UPDATE, DELETE ON GestionaleFatture.* TO 'GF_Admin'@'localhost';
 FLUSH PRIVILEGES;
+
+CREATE TABLE UserType(
+	type VARCHAR(14) PRIMARY KEY
+);
+
+CREATE TABLE User(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	password VARCHAR(255) NOT NULL,
+	username VARCHAR(255) NOT NULL,
+	nome VARCHAR(50) NOT NULL,
+	cognome VARCHAR(50) NOT NULL,
+	type_userType VARCHAR(14) NOT NULL,
+	FOREIGN KEY(type_userType) REFERENCES UserType(type) ON UPDATE CASCADE
+);
+
+CREATE TABLE UserToken(
+	user_id INT NOT NULL,
+	creationDate DATETIME NOT NULL DEFAULT NOW(),
+	value VARCHAR(255) NOT NULL,
+	FOREIGN KEY(user_id) REFERENCES User(id) ON UPDATE CASCADE,
+	PRIMARY KEY(user_id, creationDate)
+);
+
+CREATE TABLE BankAccount(
+	iban VARCHAR(34) PRIMARY KEY,
+	holder VARCHAR(255) NOT NULL,
+	street VARCHAR(50) NOT NULL,
+	streetNumber VARCHAR(50) NOT NULL,
+	postalCode INT NOT NULL,
+	city VARCHAR(50) NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	bankName VARCHAR(100) NOT NULL,
+	swift VARCHAR(100),
+	bic VARCHAR(100)
+);
+
+CREATE TABLE Company(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	phoneNumber VARCHAR(255) NOT NULL,
+	street VARCHAR(50) NOT NULL,
+	streetNumber VARCHAR(50) NOT NULL,
+	postalCode INT NOT NULL,
+	city VARCHAR(50) NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	websiteURL VARCHAR(255),
+	iconPath VARCHAR(255)
+);
+
+CREATE TABLE PrivatePerson(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	surname VARCHAR(255) NOT NULL,
+	phoneNumber VARCHAR(255) NOT NULL,
+	street VARCHAR(50) NOT NULL,
+	streetNumber VARCHAR(50) NOT NULL,
+	postalCode INT NOT NULL,
+	city VARCHAR(50) NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	iconPath VARCHAR(255)
+);
+
+CREATE TABLE WorksFor(
+	company_id INT NOT NULL,
+	privatePerson_id INT NOT NULL,
+	FOREIGN KEY(company_id) REFERENCES Company(id) ON UPDATE CASCADE,
+	FOREIGN KEY(privatePerson_id) REFERENCES PrivatePerson(id) ON UPDATE CASCADE,
+	PRIMARY KEY(company_id, privatePerson_id)
+);
+
+CREATE TABLE Invoice(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	creationDate DATETIME NOT NULL DEFAULT NOW(),
+	company_id INT,
+	privatePerson_id INT,
+	bankAccount_iban VARCHAR(34) NOT NULL,
+	user_id INT NOT NULL,
+	FOREIGN KEY(company_id) REFERENCES Company(id) ON UPDATE CASCADE,
+	FOREIGN KEY(privatePerson_id) REFERENCES PrivatePerson(id) ON UPDATE CASCADE,
+	FOREIGN KEY(bankAccount_iban) REFERENCES BankAccount(iban) ON UPDATE CASCADE,
+	FOREIGN KEY(user_id) REFERENCES User(id) ON UPDATE CASCADE
+);
+
+CREATE TABLE TaskType(
+	name VARCHAR(255) PRIMARY KEY,
+	description TEXT NOT NULL, 
+	hourlyCost FLOAT NOT NULL
+);
+
+CREATE TABLE InvoiceTask(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	description TEXT NOT NULL, 
+	hours FLOAT NOT NULL,
+	date DATETIME NOT NULL DEFAULT NOW(),
+	invoice_id INT NOT NULL,
+	taskType_name VARCHAR(255) NOT NULL,
+	FOREIGN KEY(invoice_id) REFERENCES Invoice(id) ON UPDATE CASCADE,
+	FOREIGN KEY(taskType_name) REFERENCES TaskType(name) ON UPDATE CASCADE
+);
+
+INSERT INTO UserType VALUES ("amministratore");
+INSERT INTO UserType VALUES ("impiegato");
