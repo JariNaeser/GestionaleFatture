6264eaed018291d06770b2f0bcc323bcef578dd4
Jari Näser <jari.naeser@bluewin.ch>
2022-10-13 23:16:47 +0200

    Added some features to the customer portal

--- src/frontend/GestionaleFatture-FrontEnd/src/app/components/customers/customers.component.ts
+++ src/frontend/GestionaleFatture-FrontEnd/src/app/components/customers/customers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus, faCog } from '@fortawesome/free-solid-svg-icons';
+import { CustomersService } from 'src/app/services/customersService';
 
 declare var $ :any;
 
@@ -12,19 +13,46 @@ export class CustomersComponent implements OnInit {
 
   faPlus = faPlus;
   faCog = faCog;
+  companies;
+  privatePersons;
 
-  constructor() { }
+  constructor(private customersService : CustomersService) { }
 
   ngOnInit(): void {
-    
+    this.getCompanies();
+    this.getPrivatePersons();
   }
 
   addCompany(){
     
+    this.getCompanies();
   }
 
   addPrivatePerson(){
 
+    this.getPrivatePersons();
+  }
+
+  getCompanies(){
+    this.customersService.getCompanies().subscribe(data => {
+        this.companies = data;
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getPrivatePersons(){
+    this.customersService.getPrivatePersons().subscribe(data => {
+        this.privatePersons = data;
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
+
+  findCompanyById(id : number){
+    return this.companies.find(element => element.id == id);
   }
 
 }
---
+++ src/frontend/GestionaleFatture-FrontEnd/src/app/services/customersService.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { PasswordEncrypterService } from './passwordEncrypterService';
+import { of, Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { catchError, mapTo, tap } from 'rxjs/operators';
+import { environment } from '../../environments/environment.prod';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CustomersService {
+
+    constructor(private http:HttpClient, private passwordEncrypterService:PasswordEncrypterService) {}
+
+    public getCompanies():Observable<any>{
+        return this.http.get(environment.backendServerIP + "getCompanies");
+    }
+
+    public getCompany(id : number):Observable<any>{
+        return this.http.get(environment.backendServerIP + `getCompanies/${id}`);
+    }
+
+    public getPrivatePersons():Observable<any>{
+        return this.http.get(environment.backendServerIP + "getPrivatePersons");
+    }
+
+    public getPrivatePerson(id : number):Observable<any>{
+        return this.http.get(environment.backendServerIP + `getPrivatePerson/${id}`);
+    }
+
+}
